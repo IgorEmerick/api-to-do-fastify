7a8fea9fb097cd5dde3611f424e0280cbe5ffd19
Igor Emerick <igorbarbosaemerick@gmail.com>
2023-05-17 21:12:36 -0300

    config: data source

---
+++ src/shared/infra/typeorm/data_sources/DefaultDataSource.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+
+config();
+
+export const DefaultDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: 'to-do-fastify',
+  synchronize: false,
+  entities: ['src/modules/**/infra/typeorm/entities/*.ts'],
+  migrations: ['src/shared/infra/typeorm/migrations/*.ts'],
+});
