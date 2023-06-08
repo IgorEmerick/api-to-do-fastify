95f57efe9030a6692192397e1b92c3c55769a0bb
Igor Emerick <igorbarbosaemerick@gmail.com>
2023-06-08 11:13:08 -0300

    update: server.ts to connect with database before start application

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import { app } from '@shared/infra/http/app';
+import { DefaultDataSource } from '@shared/infra/typeorm/data_sources/DefaultDataSource';
 import { config } from 'dotenv';
 
 config();
 
-app.listen({ port: Number(process.env.API_PORT) }).then(() => {
-  console.log(`Server is listening on port ${process.env.API_PORT}!`);
+DefaultDataSource.initialize().then(() => {
+  app.listen({ port: Number(process.env.API_PORT) }).then(() => {
+    console.log(`Server is listening on port ${process.env.API_PORT}!`);
+  });
 });
